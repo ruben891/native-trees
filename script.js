d538c18a17fa2bf694114a3a5a413de92881ce2e
ruben891 <37756997+ruben891@users.noreply.github.com>
2022-03-31 22:43:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function howMany(selectObject) {
+    let numberSelected = 0;
+    for (let i = 0; i < selectObject.options.length; i++) {
+        if (selectObject.options[i].selected) {
+            numberSelected++;
+        }
+    }
+    return numberSelected;
+}
+
+let btn = document.getElementById('btn');
+btn.addEventListener('click', function() {
+    alert('Número de días selecionados: ' + howMany(document.selectForm.agendaDiaria));
+});
