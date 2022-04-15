b2aeca5092c434896746e209df0c9aa57f505849
ruben891 <37756997+ruben891@users.noreply.github.com>
2022-04-14 21:14:39 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+var edad = prompt("Introduce tu edad");
+
+// Ocultar contenido
+document.getElementById("formulario").style.visibility = "hidden";
+
+
+// Si es mayor de edad
+if (Number(edad) == edad) {
+    if (edad > 18) {
+        alert("Puedes agendar tu visita");
+        document.getElementById("formulario").style.visibility = "visible";
+    }
+} else {
+    alert("Lo sentimos, eres menor de edad");
+}
+
+
 function howMany(selectObject) {
     let numberSelected = 0;
     for (let i = 0; i < selectObject.options.length; i++) {
